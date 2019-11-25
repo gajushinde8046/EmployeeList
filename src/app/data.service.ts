d81463319b0d2fefbb2361dc803d845b71ad3905
Gajanan <gajushinde8046@gmail.com>
2019-11-25 19:23:52 +0530

    Django change

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,13 +11,9 @@ import { _sanitizeUrl } from '@angular/core/src/sanitization/url_sanitizer';
 export class DataService {
   id:number
 
-  private _furl = "https://getinfo-3e2e0.firebaseio.com/employees/.json";
-  private _url =  "http://localhost:3000/employees/";
-  // private _url =  "assets/data.json/";
-  //  private deleteurl = "https://getinfo-3e2e0.firebaseio.com/employees/";
-   private _puturl = "http://localhost:3000/employees";
-
-
+  private _url =  "http://localhost:3000/employees/";          // For JSON-Server Api
+  private _purl =  'http://localhost:8000/api/details/';        //Django URL for PUT DELETE GET
+ // private _gurl =  'http://localhost:8000/api/get/';       //For Django APi URL For POST GET
 
   constructor(private http:HttpClient) { 
   }
@@ -39,6 +35,23 @@ export class DataService {
     updateData(putData):Observable<any>{
       return this.http.put<any>(this._url + putData.id  , putData)
     }
+
+    // getData():Observable<any[]>{
+    //   return this.http.get<any[]>(this._gurl)
+       
+    //  }
+ 
+    //  createData(requestData):Observable<any>{
+    //    return this.http.post<any>(this._gurl,requestData)
+    //  }
+    // deleteData(delData):Observable<any>{
+   
+    //   return this.http.delete<any>(this._purl + delData.id +'/',delData)
+    // }
+
+    // updateData(putData):Observable<any>{
+    //   return this.http.put<any>(this._purl + putData.id +'/' , putData)
+    // }
    
   }
  
